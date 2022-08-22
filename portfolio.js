676f769709b3cd67f7682b52675a4a39765fa184
notrickhere <84467942+notrickhere@users.noreply.github.com>
2022-08-22 08:55:11 -0400

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,10 @@
+const body = document.body
+const replaceText = document.createElement("div")
+div.innerText = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt placeat ipsum blanditiis, eum laborum tempora nobis architecto totam dolorum hic quaerat deleniti. Vel cupiditate accusantium in excepturi eligendi deleniti? Quia. Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt sint necessitatibus exercitationem. Fugit dolorem expedita eum error vel consequuntur, quia commodi inventore veritatis blanditiis, explicabo delectus, quam impedit magni? Cumque.'
+
+const name = document.querySelector(".myName")
+const btn = document.querySelector("button")
+
+btn.addEventListener("click", function(){
+    myName.style.color = "blue";
+})
